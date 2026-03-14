566ff017f2b605f31d6990e3d05d59ff15ed37d7
Cao Minh Nguyen Cao Minh <c.m.nguyen-1@student.tudelft.nl>
2026-03-14 18:48:53 +0100

    Add education data

---
+++ src/app/data/education.ts
@@ -0,0 +1,14 @@
+import { Education } from "../types";
+
+export const education: Education[] = [
+  {
+    institution: "TU Delft",
+    degree: "BSc Computer Science & Engineering",
+    years: "2023 – 2026",
+  },
+  {
+    institution: "Vinschool Times City",
+    degree: "Cambridge International AS & A Levels",
+    years: "2021 – 2023",
+  },
+];
